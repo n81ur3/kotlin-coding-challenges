94fda12163800cb16293fc3f3ddf0950afd61cdd
Helmut Bierbaumer <h.bierbaumer@a1.net>
2022-11-05 09:41:17 +0100

    completed AoC2017 day23 part 2

--- src/test/kotlin/adventofcode2017/Day23CoprocessorConflagrationTest.kt
+++ src/test/kotlin/adventofcode2017/Day23CoprocessorConflagrationTest.kt
@@ -24,4 +24,15 @@ class Day23CoprocessorConflagrationTest {
         assertEquals(8281, coProcessor.mulCounter)
         println("Solution for day 23 part 1: ${coProcessor.mulCounter}")
     }
+
+    @Test
+    fun solution_part2() {
+        val instructionset = file.readLines()
+        val b = instructionset[0].split(" ")[2].toInt()
+        val a = b * 100 + 100000
+        val h = (a..a + 17000 step 17).count { !(it.toBigInteger().isProbablePrime(8)) }
+
+        assertEquals(911, h)
+        println("Solution for day 23 part 2: $h")
+    }
 }
