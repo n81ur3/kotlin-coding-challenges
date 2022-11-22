d9b723b05a5671451f83d3929e5932ed77e9e63f
Helmut Bierbaumer <h.bierbaumer@a1.net>
2022-11-22 17:36:29 +0100

    completed AoC2018 day01 part2

--- src/main/kotlin/adventofcode2018/Day01CronalCalibration.kt
+++ src/main/kotlin/adventofcode2018/Day01CronalCalibration.kt
@@ -1,15 +1,25 @@
 package adventofcode2018
 
-import java.lang.reflect.Array.get
-
 class Day01CronalCalibration
 
 class FrequencyDisplay(input: List<String>) {
     val adjustments: List<Int>
+    val recordedFrequencies = mutableSetOf<Int>()
     val finalFrequency: Int
         get() = adjustments.sum()
 
     init {
         adjustments = input.map { it.toInt() }
+    }
+
+    fun findFirstDuplicatedFrequency(): Int {
+        var currentFrequency = 0
+        while (true) {
+            adjustments.forEach {
+                currentFrequency += it
+                if (currentFrequency in recordedFrequencies) return currentFrequency
+                recordedFrequencies.add(currentFrequency)
+            }
+        }
     }
 }
--- src/test/kotlin/adventofcode2018/Day01CronalCalibrationTest.kt
+++ src/test/kotlin/adventofcode2018/Day01CronalCalibrationTest.kt
@@ -41,11 +41,68 @@ class Day01CronalCalibrationTest {
     }
 
     @Test
+    fun runSamplesPart2() {
+        val input = mutableListOf(
+            "+1",
+            "-2",
+            "+3",
+            "+1"
+        )
+
+        var frequencyDisplay = FrequencyDisplay(input)
+        val duplicatedFrequency = frequencyDisplay.findFirstDuplicatedFrequency()
+        assertEquals(2, duplicatedFrequency)
+
+        input.clear()
+        input.add("+1")
+        input.add("-1")
+        frequencyDisplay = FrequencyDisplay(input)
+        assertEquals(0, frequencyDisplay.finalFrequency)
+
+        input.clear()
+        input.add("+3")
+        input.add("+3")
+        input.add("+4")
+        input.add("-2")
+        input.add("-4")
+        frequencyDisplay = FrequencyDisplay(input)
+        assertEquals(10, frequencyDisplay.findFirstDuplicatedFrequency())
+
+        input.clear()
+        input.add("-6")
+        input.add("+3")
+        input.add("+8")
+        input.add("+5")
+        input.add("-6")
+        frequencyDisplay = FrequencyDisplay(input)
+        assertEquals(5, frequencyDisplay.findFirstDuplicatedFrequency())
+
+        input.clear()
+        input.add("+7")
+        input.add("+7")
+        input.add("-2")
+        input.add("-7")
+        input.add("-4")
+        frequencyDisplay = FrequencyDisplay(input)
+        assertEquals(14, frequencyDisplay.findFirstDuplicatedFrequency())
+    }
+
+    @Test
     fun solutionPart1() {
         val input = file.readLines()
         val frequencyDisplay = FrequencyDisplay(input)
 
         assertEquals(536, frequencyDisplay.finalFrequency)
         println("Solution for AoC2018-Day01-Part01: ${frequencyDisplay.finalFrequency}")
+    }
+
+    @Test
+    fun solutionPart2() {
+        val input = file.readLines()
+        val frequencyDisplay = FrequencyDisplay(input)
+        val firstDuplicatedFrequency = frequencyDisplay.findFirstDuplicatedFrequency()
+
+        assertEquals(75108, firstDuplicatedFrequency)
+        println("Solution for AoC2018-Day01-Part02: $firstDuplicatedFrequency")
     }
 }
