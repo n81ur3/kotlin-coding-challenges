b0ffd4f389eacfbf8edfc48287238ca1f7d1155e
Helmut Bierbaumer <h.bierbaumer@a1.net>
2023-04-01 09:48:54 +0200

    completed AoC2018 day23 part2

--- src/main/kotlin/adventofcode2018/Day23ExperimentalEmergencyTeleportation.kt
+++ src/main/kotlin/adventofcode2018/Day23ExperimentalEmergencyTeleportation.kt
@@ -4,12 +4,22 @@ import kotlin.math.abs
 
 class Day23ExperimentalEmergencyTeleportation
 
+data class TeleportCoordinate(val x: Int, val y: Int, val z: Int)
+
 data class Nanobot(val x: Int, val y: Int, val z: Int, val radius: Int) {
 
     fun inRange(otherBot: Nanobot): Boolean {
-        val manhattanDistance = abs(otherBot.x - x) + abs(otherBot.y - y) + abs(otherBot.z - z)
+        val manhattanDistance = distanceTo(otherBot.x, otherBot.y, otherBot.z)
         return manhattanDistance <= otherBot.radius
     }
+
+    fun withinRangeOfSharedPoint(otherBot: Nanobot) =
+        distanceTo(otherBot.x, otherBot.y, otherBot.z) <= radius + otherBot.radius
+
+    fun inRange(xDist: Int, yDist: Int, zDist: Int) = distanceTo(xDist, yDist, zDist) <= radius
+
+    internal fun distanceTo(xDist: Int, yDist: Int, zDist: Int) =
+        abs(x - xDist) + abs(y - yDist) + abs(z - zDist)
 }
 
 class TeleportationDevice(val input: List<String>) {
@@ -28,4 +38,62 @@ class TeleportationDevice(val input: List<String>) {
     }
 
     fun numberOfBotsInRangeOfStrongest() = bots.count { it.inRange(strongestBot) }
+
+    fun strongestCoordinate(): TeleportCoordinate {
+        var strongest = TeleportCoordinate(0, 0, 0)
+        var strongestCount = 0
+
+        val minX = bots.minOf { it.x }
+        val maxX = bots.maxOf { it.x }
+        val minY = bots.minOf { it.y }
+        val maxY = bots.maxOf { it.y }
+        val minZ = bots.minOf { it.z }
+        val maxZ = bots.maxOf { it.z }
+
+        var botsCount: Int
+        (minX..maxX).forEach { x ->
+            (minY..maxY).forEach { y ->
+                (minZ..maxZ).forEach { z ->
+                    botsCount = bots.count { it.inRange(x, y, z) }
+                    if (botsCount > strongestCount) {
+                        strongestCount = botsCount
+                        strongest = TeleportCoordinate(x, y, z)
+                    }
+                }
+            }
+        }
+        return strongest
+    }
+
+    fun strongestDistance(): Int {
+        val neighbors: Map<Nanobot, Set<Nanobot>> = bots.map { bot ->
+            Pair(bot, bots.filterNot { it == bot }.filter { bot.withinRangeOfSharedPoint(it) }.toSet())
+        }.toMap()
+
+        val clique: Set<Nanobot> = BronKerbosch(neighbors).largestClique()
+        return clique.map { it.distanceTo(0, 0, 0) - it.radius }.max()
+    }
+}
+
+class BronKerbosch<T>(val neighbors: Map<T, Set<T>>) {
+
+    private var bestR: Set<T> = emptySet()
+
+    fun largestClique(): Set<T> {
+        execute(neighbors.keys)
+        return bestR
+    }
+
+    private fun execute(p: Set<T>, r: Set<T> = emptySet(), x: Set<T> = emptySet()) {
+        if (p.isEmpty() && x.isEmpty()) {
+            if (r.size > bestR.size) bestR = r
+        } else {
+            val mostNeighborsOfPandX: T = (p + x).maxBy { neighbors.getValue(it).size }!!
+            val pWithoutNeighbors = p.minus(neighbors[mostNeighborsOfPandX]!!)
+            pWithoutNeighbors.forEach { v ->
+                val neighborsOfV = neighbors[v]!!
+                execute(p.intersect(neighborsOfV), r + v, x.intersect(neighborsOfV))
+            }
+        }
+    }
 }
--- src/test/kotlin/adventofcode2018/Day23ExperimentalEmergencyTeleportationTest.kt
+++ src/test/kotlin/adventofcode2018/Day23ExperimentalEmergencyTeleportationTest.kt
@@ -35,6 +35,22 @@ class Day23ExperimentalEmergencyTeleportationTest {
     }
 
     @Test
+    fun runSamplePart2() {
+        val input = listOf(
+            "pos=<10,12,12>, r=2",
+            "pos=<12,14,12>, r=2",
+            "pos=<16,12,12>, r=4",
+            "pos=<14,14,14>, r=6",
+            "pos=<50,50,50>, r=200",
+            "pos=<10,10,10>, r=5"
+        )
+        val teleportationDevice = TeleportationDevice(input)
+
+        val bestCoordinate = teleportationDevice.strongestCoordinate()
+        assertEquals(TeleportCoordinate(12, 12, 12), bestCoordinate)
+    }
+
+    @Test
     fun solutionPart1() {
         val input = file.readLines()
         val teleportationDevice = TeleportationDevice(input)
@@ -43,5 +59,16 @@ class Day23ExperimentalEmergencyTeleportationTest {
 
         assertEquals(640, numberOfBotsInRangeOfStrongest)
         println("Solution for AoC2018-Day23-Part01: $numberOfBotsInRangeOfStrongest")
+    }
+
+    @Test
+    fun solutionPart2() {
+        val input = file.readLines()
+        val teleportationDevice = TeleportationDevice(input)
+
+        val strongestDistance = teleportationDevice.strongestDistance()
+
+        assertEquals(113066145, strongestDistance)
+        println("Solution for AoC2018-Day23-Part02: $strongestDistance")
     }
 }
