ea51001b8b3027d46862d92c07e5c649e4d78b96
Helmut Bierbaumer <h.bierbaumer@a1.net>
2023-05-11 22:05:16 +0200

    completed AoC2019 day05 part2

--- src/test/kotlin/adventofcode2019/Day05SunnyAstroidsTest.kt
+++ src/test/kotlin/adventofcode2019/Day05SunnyAstroidsTest.kt
@@ -17,12 +17,51 @@ class Day05SunnyAstroidsTest {
     @Test
     fun solutionPart1() {
         val input = file.readLines()[0]
-        val airConditionUnit = AirConditionUnit(input)
+        val shipComputer = ShipComputer(input)
 
-        val finalOutput = airConditionUnit.run()
+        val finalOutput = shipComputer.run(1)
 
         assertEquals(15508323, finalOutput)
         println("Solution for AoC2019-Day05-Part01: $finalOutput")
+    }
+
+    @Test
+    fun runSamplesPart2() {
+        var shipComputer = ShipComputer("3,9,8,9,10,9,4,9,99,-1,8")
+
+        println("Output sample part1 - equals Input(5): ${shipComputer.run(5)}")
+        shipComputer = ShipComputer("3,9,8,9,10,9,4,9,99,-1,8")
+        println("Output sample part1 - equals Input(8): ${shipComputer.run(8)}")
+
+        shipComputer = ShipComputer("3,9,7,9,10,9,4,9,99,-1,8")
+        println("Output sample part1 - less than Input(5): ${shipComputer.run(5)}")
+        shipComputer = ShipComputer("3,9,7,9,10,9,4,9,99,-1,8")
+        println("Output sample part1 - less than Input(9): ${shipComputer.run(9)}")
+
+        shipComputer = ShipComputer("3,3,1108,-1,8,3,4,3,99")
+        println("Output sample part1 - equal Input(5): ${shipComputer.run(5)}")
+        shipComputer = ShipComputer("3,3,1108,-1,8,3,4,3,99")
+        println("Output sample part1 - equal Input(8): ${shipComputer.run(8)}")
+
+        shipComputer = ShipComputer("3,3,1107,-1,8,3,4,3,99")
+        println("Output sample part1 - less than Input(5): ${shipComputer.run(5)}")
+        shipComputer = ShipComputer("3,3,1107,-1,8,3,4,3,99")
+        println("Output sample part1 - less than Input(9): ${shipComputer.run(9)}")
+
+        shipComputer = ShipComputer("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9")
+        println("Output sample part1 - jump Input(0): ${shipComputer.run(0)}")
+        shipComputer = ShipComputer("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9")
+        println("Output sample part1 - jump Input(9): ${shipComputer.run(9)}")
+    }
+
+    @Test
+    fun solutionPart2() {
+        val input = file.readLines()[0]
+        val shipComputer = ShipComputer(input)
+
+        val finalOutput = shipComputer.run(5)
+        assertEquals(9006327, finalOutput)
+        println("Solution for AoC2019-Day05-Part02: $finalOutput")
     }
 
 }
