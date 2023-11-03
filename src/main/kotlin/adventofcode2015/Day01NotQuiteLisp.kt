dc3545c591936262246af2645861e08353111df4
Helmut Bierbaumer <h.bierbaumer@a1.net>
2023-11-03 21:35:50 +0100

    completed AoC2015 day01 part2

--- src/main/kotlin/adventofcode2015/Day01NotQuiteLisp.kt
+++ src/main/kotlin/adventofcode2015/Day01NotQuiteLisp.kt
@@ -5,6 +5,19 @@ class Day01NotQuiteLisp
 class ParenthesisParser(val floorMap: String) {
 
     fun getFinalFloor(): Int {
-        return floorMap.fold(0) { floor, value -> if (value == '(') floor + 1 else floor - 1}
+        return floorMap.fold(0) { floor, value -> if (value == '(') floor + 1 else floor - 1 }
+    }
+
+    fun getFirstBasementStep(): Int {
+        return floorMap.foldIndexed(0) { index, floor, value ->
+            if (value == '(') {
+                return@foldIndexed floor + 1
+            } else {
+                if (floor == 0) {
+                    return index + 1
+                }
+                return@foldIndexed floor - 1
+            }
+        }
     }
 }
--- src/test/kotlin/adventofcode2015/Day01NotQuiteLispTest.kt
+++ src/test/kotlin/adventofcode2015/Day01NotQuiteLispTest.kt
@@ -2,6 +2,9 @@ package adventofcode2015
 
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import utils.ResourceLoader
 import java.io.File
 import kotlin.test.assertEquals
@@ -14,6 +17,20 @@ class Day01NotQuiteLispTest {
         file = ResourceLoader.getFile("aoc2015/aoc2015_day01_input.txt")
     }
 
+    @ParameterizedTest
+    @MethodSource("sampleTestData1")
+    fun runSamplesPart1(floorMap: String, expectedResult: Int) {
+        val parenthesisParser = ParenthesisParser(floorMap)
+        assertEquals(expectedResult, parenthesisParser.getFinalFloor())
+    }
+
+    @ParameterizedTest
+    @MethodSource("sampleTestData2")
+    fun runSamplesPart2(floorMap: String, expectedResult: Int) {
+        val parenthesisParser = ParenthesisParser(floorMap)
+        assertEquals(expectedResult, parenthesisParser.getFirstBasementStep())
+    }
+
     @Test
     fun solutionPart1() {
         val flooMap = file.readLines()[0]
@@ -23,5 +40,34 @@ class Day01NotQuiteLispTest {
 
         assertEquals(138, finalFloor)
         println("Solution for AoC2015-Day01-Part01: $finalFloor")
+    }
+
+    @Test
+    fun solutionPart2() {
+        val floorMap = file.readLines()[0]
+        val parenthesisParser = ParenthesisParser(floorMap)
+
+        val firstBasementStep = parenthesisParser.getFirstBasementStep()
+
+        println("Solution for AoC2015-Day01-Part02: $firstBasementStep")
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun sampleTestData1() = listOf(
+            Arguments.of("(())", 0),
+            Arguments.of("(()(()(", 3),
+            Arguments.of("))(((((", 3),
+            Arguments.of("())", -1),
+            Arguments.of(")())())", -3),
+        )
+
+        @JvmStatic
+        fun sampleTestData2() = listOf(
+            Arguments.of(")", 1),
+            Arguments.of("()())", 5),
+        )
+
     }
 }
