b38058e1443b5905ab4f9921d23323d33868f4b9
Helmut Bierbaumer <h.bierbaumer@a1.net>
2024-10-02 08:07:24 +0200

    completed AoC2016 day05 part2

--- src/main/kotlin/adventofcode2016/Day05NiceGameOfChess.kt
+++ src/main/kotlin/adventofcode2016/Day05NiceGameOfChess.kt
@@ -3,23 +3,40 @@ package adventofcode2016
 import java.math.BigInteger
 import java.security.MessageDigest
 
-class Day05NiceGameOfChess {
-}
-
 class Md5Station {
 
     fun findPassword(doorID: String): String {
-        var password = StringBuilder()
+        val password = StringBuilder()
         var counter = 0
         var hash = ""
         while (password.length < 8) {
-            hash = getHash("$doorID$counter")
+            hash = getHash(doorID + counter)
             if (hash.startsWith("00000")) {
                 password.append(hash[5])
             }
             counter++
         }
         return password.toString()
+    }
+
+    fun findBetterPassword(doorID: String): String {
+        val password = mutableListOf('_', '_', '_', '_', '_', '_', '_', '_')
+        var counter = 0
+        var hash = ""
+        while (password.contains('_')) {
+            hash = getHash(doorID + counter)
+            if (hash.startsWith("00000")) {
+                if (hash[5] in '0'..'7') {
+                    val position = hash[5].digitToInt()
+                    if (password[position] == '_') {
+                        password[position] = hash[6]
+                    }
+                }
+            }
+            counter++
+        }
+
+        return password.joinToString(separator = "")
     }
 
     fun getHash(input: String): String {
--- src/test/kotlin/adventofcode2016/Day05NiceGameOfChessTest.kt
+++ src/test/kotlin/adventofcode2016/Day05NiceGameOfChessTest.kt
@@ -23,11 +23,26 @@ class Day05NiceGameOfChessTest {
     }
 
     @Test
+    fun runSamplePart2() {
+        val md5Station = Md5Station()
+        val password = md5Station.findBetterPassword("abc")
+        assertEquals("05ace8e3", password)
+    }
+
+    @Test
     fun solutionPart1() {
         val md5Station = Md5Station()
         val password = md5Station.findPassword("wtnhxymk")
-        println(password)
         assertEquals("2414bc77", password)
         println("Solution for AoC2016-Day05-Part01: $password")
+    }
+
+    @Test
+    fun solutionPart2() {
+        val md5Station = Md5Station()
+        val password = md5Station.findBetterPassword("wtnhxymk")
+        println(password)
+        assertEquals("437e60fc", password)
+        println("Solution for AoC2016-Day05-Part02: $password")
     }
 }
