c56ef819459dede682dc3e08d40717bb148edcf3
Helmut Bierbaumer <h.bierbaumer@a1.net>
2025-02-10 06:16:24 +0100

    completed AoC2024 day14 part1

---
+++ src/main/kotlin/adventofcode2024/Day14RestroomRedoubt.kt
@@ -0,0 +1,58 @@
+package adventofcode2024
+
+data class SecurityBot(
+    var x: Int,
+    var y: Int,
+    var vx: Int,
+    var vy: Int
+) {
+    fun move(width: Int, height: Int) {
+        x = (x + vx) % width
+        y = (y + vy) % height
+        if (x < 0) x = width + x
+        if (y < 0) y = height + y
+    }
+}
+
+class EBHQRestroom(input: List<String>, val width: Int, val height: Int) {
+    val bots: List<SecurityBot>
+    val left = IntRange(0, width / 2 - 1)
+    val right = IntRange(width / 2 + 1, width)
+    val top = IntRange(0, height / 2 - 1)
+    val bottom = IntRange(height / 2 + 1, height)
+
+    init {
+        bots = input.map { parseRobot(it) }
+    }
+
+    private fun parseRobot(input: String): SecurityBot {
+        val x = input.substringAfter("=").substringBefore(",").toInt()
+        val y = input.substringAfter(",").substringBefore(" ").toInt()
+        val vx = input.substringAfter("v=").substringBefore(",").toInt()
+        val vy = input.substringAfterLast(",").toInt()
+        return SecurityBot(x, y, vx, vy)
+    }
+
+    fun moveRobots(seconds: Int) {
+        repeat(seconds) { bots.forEach { it.move(width, height) } }
+    }
+
+    fun quarterCounts(): Int {
+        val topLeft = bots.count { left.contains(it.x) && top.contains(it.y) }
+        val topRight = bots.count { right.contains(it.x) && top.contains(it.y) }
+        val bottomLeft = bots.count { left.contains(it.x) && bottom.contains(it.y) }
+        val bottomRight = bots.count { right.contains(it.x) && bottom.contains(it.y) }
+        return topLeft * topRight * bottomLeft * bottomRight
+    }
+
+    fun printLayout() {
+        (0 until height).forEach { y ->
+            (0 until width).forEach { x ->
+                val botCount = bots.count { it.y == y && it.x == x }
+                if (botCount > 0) print(botCount)
+                else print(".")
+            }
+            println()
+        }
+    }
+}
---
+++ src/test/kotlin/adventofcode2024/Day14RestroomRedoubtTest.kt
@@ -0,0 +1,96 @@
+package adventofcode2024
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import utils.ResourceLoader
+import java.io.File
+import kotlin.test.assertEquals
+
+class Day14RestroomRedoubtTest {
+    lateinit var file: File
+
+    @BeforeEach
+    fun setup() {
+        file = ResourceLoader.getFile("aoc2024/aoc2024_day14_input.txt")
+    }
+
+    @ParameterizedTest
+    @MethodSource("sampleTestData1")
+    fun runSamplesPart1(input: List<String>, expectedResult: Int) {
+        val restroom = EBHQRestroom(input, 11, 7)
+        restroom.moveRobots(100)
+        assertEquals(expectedResult, restroom.quarterCounts())
+    }
+
+    @ParameterizedTest
+    @MethodSource("sampleTestData2")
+    fun runSamplesPart2(input: List<String>, expectedResult: Long) {
+        val clawArcade = ClawArcade(input)
+        assertEquals(expectedResult, clawArcade.calculateFewestTokensAdvanced())
+    }
+
+    @Test
+    fun solutionPart1() {
+        val restroom = EBHQRestroom(file.readLines(), 101, 103)
+        restroom.moveRobots(100)
+        val solution = restroom.quarterCounts()
+        assertEquals(220971520, solution)
+        println("Solution for AoC2024-Day14-Part01: $solution")
+    }
+
+    @Test
+    fun solutionPart2() {
+        val clawArcade = ClawArcade(file.readLines())
+        val solution = clawArcade.calculateFewestTokensAdvanced()
+        assertEquals(94955433618919, solution)
+        println("Solution for AoC2024-Day14-Part02: $solution")
+    }
+
+
+    companion object {
+
+        @JvmStatic
+        fun sampleTestData1() = listOf(
+            Arguments.of(
+                """
+                    p=0,4 v=3,-3
+                    p=6,3 v=-1,-3
+                    p=10,3 v=-1,2
+                    p=2,0 v=2,-1
+                    p=0,0 v=1,3
+                    p=3,0 v=-2,-2
+                    p=7,6 v=-1,-3
+                    p=3,0 v=-1,-2
+                    p=9,3 v=2,3
+                    p=7,3 v=-1,2
+                    p=2,4 v=2,-3
+                    p=9,5 v=-3,-3
+                    """.trimIndent().lines(), 12
+            )
+        )
+
+        @JvmStatic
+        fun sampleTestData2() = listOf(
+            Arguments.of(
+                """
+                    p=0,4 v=3,-3
+                    p=6,3 v=-1,-3
+                    p=10,3 v=-1,2
+                    p=2,0 v=2,-1
+                    p=0,0 v=1,3
+                    p=3,0 v=-2,-2
+                    p=7,6 v=-1,-3
+                    p=3,0 v=-1,-2
+                    p=9,3 v=2,3
+                    p=7,3 v=-1,2
+                    p=2,4 v=2,-3
+                    p=9,5 v=-3,-3
+                    """.trimIndent().lines(), 875318608908
+            )
+        )
+    }
+
+}
